691389a16600eab97b75506f6daf946319e8b51a
Jeb Bailey <jibailey@caltech.edu>
2025-03-04 17:07:19 -0800

    Tweak all

--- cooethercat/__init__.py
+++ cooethercat/__init__.py
@@ -1,4 +1,4 @@
-from .epos4 import EPOS4Bus, EPOS4MicroTRB_12CC_Config
+from .epos4 import EPOS4Bus, EPOS4MicroTRB_12CC_Config, EPOS4Motor
 from .helpers import StatuswordStates
 
-__all__ = [EPOS4Bus, StatuswordStates, EPOS4MicroTRB_12CC_Config]
+__all__ = [EPOS4Bus, StatuswordStates, EPOS4MicroTRB_12CC_Config, EPOS4Motor]
