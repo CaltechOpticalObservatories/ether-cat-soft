55b2ac4bda0bc68d4e625c3bdf8d886375e46ea9
Jeb Bailey <jibailey@caltech.edu>
2025-03-11 15:57:38 -0700

    Remove EPOS4MicroTRB_12CC_Config import and reference.

--- cooethercat/__init__.py
+++ cooethercat/__init__.py
@@ -1,4 +1,4 @@
-from .epos4 import EPOS4Bus, EPOS4MicroTRB_12CC_Config, EPOS4Motor
+from .epos4 import EPOS4Bus, EPOS4Motor
 from .helpers import StatuswordStates
 
-__all__ = [EPOS4Bus, StatuswordStates, EPOS4MicroTRB_12CC_Config, EPOS4Motor]
+__all__ = [EPOS4Bus, StatuswordStates, EPOS4Motor]
